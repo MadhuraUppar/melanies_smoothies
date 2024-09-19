83729b4aef2404e8d76c7783342948751804b0ec
MadhuraUppar <149491686+MadhuraUppar@users.noreply.github.com>
2024-09-20 00:16:41 +0530

    Update streamlit_app.py
    removed snowflake session

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,13 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title(":cup_with_straw: Example Streamlit App :cup_with_straw:")
 name_on_smoothie = st.text_input("Name on Smoothie")
 st.write("The Name on the smoothie will be", name_on_smoothie)
 st.write("Choose the fruits you want to custom smoothie")
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 ingredients = st.multiselect(
     "Choose upto 5 ingredients",
